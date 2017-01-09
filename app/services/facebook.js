8f3adc0457361a6334adb4cf5fa537e785be4f04
Robbie Speed <robert.y.speed@gmail.com>
2017-01-09 19:55:33 -0400

    added list of likes

--- app/services/facebook.js
+++ app/services/facebook.js
@@ -4,7 +4,7 @@ import ENV from 'social-app-example/config/environment';
 const { computed, RSVP } = Ember;
 
 export default Ember.Service.extend({
-  loggedIn: computed.equal('loginStatus', 'connected'),
+  loggedIn: computed.equal('loginStatus', 'connected').readOnly(),
 
   init () {
     return new RSVP.Promise((resolve) => {
@@ -33,6 +33,8 @@ export default Ember.Service.extend({
   },
 
   login () {
+    const scope = 'user_friends,user_likes,user_actions.video,user_actions.books,' +
+      'user_actions.music,user_actions.fitness,user_tagged_places';
     FB.login((response) => {
       console.log(response);
       const loginStatus = this.set('loginStatus', response.status);
@@ -40,26 +42,30 @@ export default Ember.Service.extend({
         this.getUserData(response.authResponse.userID);
         // Logged into your app and Facebook.
       }
-    }, { return_scopes: true });
+    }, { return_scopes: true, scope });
   },
 
   getUserData (userID) {
     const user = this.set('user', Ember.Object.create({ id: userID }));
 
-    FB.api(`/${userID}?fields=first_name,last_name`, (data) => {
+    FB.api(`/${userID}?fields=first_name,last_name,likes`, (data) => {
+      console.log(data);
       user.set('firstName', data.first_name);
       user.set('lastName', data.last_name);
+      user.set('likes', data.likes.data);
     });
 
     FB.api(`/${userID}/picture?height=64&width=64`, (data) => {
       user.set('picture', data.data.url);
     });
+
+
   },
 
   logout () {
     FB.logout((response) => {
       console.log(response);
-      this.set('loggedIn', false);
+      this.set('loginStatus', undefined);
     });
   }
 });
